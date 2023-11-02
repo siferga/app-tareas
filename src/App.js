96fe8b8380dd3813b0f075efec3618a2791a5639
Siferga <sifergadeveloper@gmail.com>
2023-11-02 15:35:14 +0100

    task

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import changelogo from  './images/changelogo.jpg'
 
 function App() {
   return (
-    <div className="App">
-      Hi
+    <div className='app-tareas'>
+      <div className='change-logo-container'>
+        <img
+          src={changelogo}
+          className='change-logo'
+        />
+      </div>
+      <div className='main-task-list'>
+        <h1>Mis Tareas</h1>
+    
+      </div>
     </div>
   );
 }
